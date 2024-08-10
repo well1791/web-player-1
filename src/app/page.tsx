7f75dbf538a5a17b7e112f1a1c2d8ef80e568ec3
Wellington Mendoza <wellsaint91@gmail.com>
2024-08-10 23:32:21 +0200

    feat: store user input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [searchValue, setInputValue] = useState("");
+
+  console.log({ searchValue });
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <div className={styles.searchBarContainer}>
           <span className={styles.icon}>icon</span>
-          <input className={styles.searchBar} type="text" />
+          <input
+            className={styles.searchBar}
+            type="text"
+            onChange={(event) => {
+              setInputValue(event.target.value);
+            }}
+          />
         </div>
         <div className={styles.rectangle}>
           <select className={styles.orderButton}>
