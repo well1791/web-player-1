5fe4936038978d68b3b444bcd4ff9d3f5377e18a
Wellington Mendoza <wellsaint91@gmail.com>
2024-08-10 23:40:17 +0200

    feat: move components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,13 @@
 "use client";
 
-import { useState } from "react";
-
 import styles from "./page.module.css";
+import SearchBar from "./components/searchBar/searchBar";
 
 export default function Home() {
-  const [searchValue, setInputValue] = useState("");
-
-  console.log({ searchValue });
-
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <div className={styles.searchBarContainer}>
-          <span className={styles.icon}>icon</span>
-          <input
-            className={styles.searchBar}
-            type="text"
-            onChange={(event) => {
-              setInputValue(event.target.value);
-            }}
-          />
-        </div>
+        <SearchBar />
         <div className={styles.rectangle}>
           <select className={styles.orderButton}>
             <option disabled>Order By</option>
