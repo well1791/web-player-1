c0b4d7bfc16f436939a2b2ac58845b5ad12f3a37
Leotroya <leotroya19@gmail.com>
2024-09-01 10:33:18 -0500

    The Footer section #11 is properly composed of the correct components.

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react";
+
+import styles from "./footer.module.css";
+
+export default function Footer() {
+  const [] = useState("");
+
+    return(
+        <div>
+            <footer className={styles.footer}>footer</footer>
+        </div>
+    )
+}
