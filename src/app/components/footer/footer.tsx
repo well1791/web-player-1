99dff7da92284a949de5ed0aad6d3f05d7208bcb
Hector1989 <leotroya19@gmail.com>
2024-09-02 12:38:11 -0500

    Deleted extra line
    the line 'const [] = useState("");' was deleted, it's innecesary

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -5,8 +5,7 @@ import { useState } from "react";
 import styles from "./footer.module.css";
 
 export default function Footer() {
-  const [] = useState("");
-
+  
     return(
         <div>
             <footer className={styles.footer}>footer</footer>
