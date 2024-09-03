77bd759cfda010dd3ea47f6a13bc87a953518225
Leotroya <leotroya19@gmail.com>
2024-09-03 12:00:38 -0500

    volume pt1

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -4,11 +4,15 @@ import { useState } from "react";
 
 import styles from "./footer.module.css";
 
+import Volume from "../volume/volume";
+
 export default function Footer() {
   
     return(
         <div>
-            <footer className={styles.footer}>footer</footer>
+            <footer className={styles.footer}>
+                <Volume />
+            </footer>  
         </div>
     )
 }
